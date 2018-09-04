174029e1bcbd20bfa3501cfec8d82ac1be0ef715
Justin Dressel <dressel@chapman.edu>
2018-09-04 08:01:45 -0700

    Whoops

---
+++ broken_script.sh
@@ -0,0 +1,12 @@
+#/bin/bsh
+
+# You will change this line later
+eco "This script hs typs. Pleez fix them."
+# You will also change this line later
+
+echo "Positive integers from 005 to 100:"
+for i in ${seq -w 5 100); do
+    echo i
+fi
+
+echo "This script is now fixed."
