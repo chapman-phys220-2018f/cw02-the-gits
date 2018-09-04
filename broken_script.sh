2ba5cf4b4a59c01031f7cc5ba2955c794c175e25
wheatonabigail <wheaton@chapman.edu>
2018-09-04 17:54:40 +0000

    Fixed Script

--- broken_script.sh
+++ broken_script.sh
@@ -1,12 +1,12 @@
-#/bin/bsh
+#!/bin/bash
 
 # You will change this line later
-eco "This script hs typs. Pleez fix them."
+echo "This script hs typos. Pleez fix them."
 # You will also change this line later
 
 echo "Positive integers from 005 to 100:"
-for i in ${seq -w 5 100); do
-    echo i
-fi
+for i in $(seq -w 5 100); do
+    echo $i
+done
 
 echo "This script is now fixed."
